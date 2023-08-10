d09a6a4a2edcd2282864daa14adb5047605c84b8
wafaj3fr <wafaagaafar@gmail.com>
2023-08-10 15:09:53 -0700

    edits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
-int main(){
+/* main - Entry point of the program */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
