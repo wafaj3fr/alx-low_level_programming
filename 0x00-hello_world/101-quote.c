d4f6f6b62bd5efbfc42b65a39fca9bc073959130
wafaj3fr <wafaagaafar@gmail.com>
2023-08-10 18:36:13 -0700

    added 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 1 means success
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
