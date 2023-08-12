a89ad75caf2700c79bdea7fae6c9f369005f6f4f
wafaj3fr <wafaagaafar@gmail.com>
2023-08-11 18:59:51 -0700

    added 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poin
+ *
+ * Return: 0 means success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	return (0);
+}
