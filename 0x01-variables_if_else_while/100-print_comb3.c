e525dc72e371ef5a25177658c74bb09e8b7a0c1f
wafaj3fr <wafaagaafar@gmail.com>
2023-08-13 12:22:14 -0700

    edits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -12,14 +10,14 @@ int main(void)
 {
 	int i, j;
 
-	for (i = '0' ; i < '9'; i++)
+	for (i = 0 ; i < 9; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 			if (j != i)
 			{
-				putchar(i);
-				putchar(j);
+				putchar(i + '0');
+				putchar(j + '0');
 				if (i == '8' && j == '9')
 					continue;
 				putchar(',');
