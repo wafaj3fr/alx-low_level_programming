c387493988bb07e0710984e8e18304b13ed907b4
wafaj3fr <wafaagaafar@gmail.com>
2023-08-13 13:28:00 -0700

    edits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,9 +12,9 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 1; j < 9 ; j++)
+		for (j = i + 1; j < 9 ; j++)
 		{
-			for (k = 2; k < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				if ((j != i) != k)
 				{
