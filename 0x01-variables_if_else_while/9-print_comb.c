6863a8624794a1ba24673ca0c0a21a1ce55d2d2d
wafaj3fr <wafaagaafar@gmail.com>
2023-08-13 07:34:34 -0700

    edits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,13 +13,8 @@ int main(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		putchar(i + '0');
-		if (i == 9)
-			continue;
-		else
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
