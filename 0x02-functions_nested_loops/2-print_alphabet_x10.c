d2719905238c3fe2e4e7fcb4d641088734c52774
wafaj3fr <wafaagaafar@gmail.com>
2023-08-15 14:19:13 -0700

    edits

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,13 @@
 
 void print_alphabet_x10(void)
 {
-	int i = 0, c = 'a';
+	int i = 0, c;
 
 	while (i < 10)
 	{
-		while (c <= 'z')
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-			c++;
 		}
 		_putchar('\n');
 	}
