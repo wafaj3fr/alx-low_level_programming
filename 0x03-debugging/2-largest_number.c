a138e530d02a642673ce9374b4ea0f68986e9adb
wafaj3fr <wafaagaafar@gmail.com>
2023-08-16 15:25:43 -0700

    edits

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,9 +12,9 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (((a != b) != c) && (a > b && a  > c))
+	if (a >= b && a  >= c)
 		largest = a;
-	else if (b > a && a > c)
+	else if (b >= a && a >= c)
 		largest = b;
 	else
 		largest = c;
