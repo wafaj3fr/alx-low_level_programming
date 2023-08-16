befac6631275725220146bac1806ab1bf784378a
wafaj3fr <wafaagaafar@gmail.com>
2023-08-16 14:53:59 -0700

    betty

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -12,6 +12,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	
 	return (0);
 }
