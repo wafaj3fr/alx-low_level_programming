54abbfde8a40159a2271b94178acf10b37cf41e5
wafaj3fr <wafaagaafar@gmail.com>
2023-08-15 19:31:10 -0700

    edits

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: natural number
+ * Return: natural numbers to 98
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar
