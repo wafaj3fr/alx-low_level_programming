25ac12b18223edf8120b6388de0c6434974683f3
wafaj3fr <wafaagaafar@gmail.com>
2023-08-15 18:50:08 -0700

    added 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,16 @@
 void jack_bauer(void)
 {
 	int m, h;
-	for (h = 0; h < 24; h++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (m = 0; m < 60; m++)
+		for (m = 0; m <= 59; m++)
 		{
 			_putchar((h/10) + '0');
 			_putchar((h%10) + '0');
 			_putchar(':');
 			_putchar((m/10) + '0');
 			_putchar((m%10) + '0');
+			_putchar(10);
 		}
 	}
 }
