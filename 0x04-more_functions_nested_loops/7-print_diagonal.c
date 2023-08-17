efaa63b577bc786d96fb3bea222ec41177f2c8cd
wafaj3fr <wafaagaafar@gmail.com>
2023-08-17 13:15:04 -0700

    edits

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,16 +8,16 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
+
+	if (i <= 0)
+	{
+		_putchar('\n');
+	}
+
 
 	for (i = 0; i < n; i++)
 	{
-		int j;
-
-		if (i <= 0)
-		{
-			_putchar('\n');
-		}
-
 		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
