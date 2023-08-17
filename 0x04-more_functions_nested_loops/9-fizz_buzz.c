7e8a7a1dde6e080393f4c37f37cbbb258cd72f5b
wafaj3fr <wafaagaafar@gmail.com>
2023-08-17 14:40:49 -0700

    added 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <=100)
+	{
+		if (i % 3 ==0 && i % 5 ==0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+		}
+		else
+		{
+			putchar(i);
+			putchar(' ');
+		}
+	}
+}	
