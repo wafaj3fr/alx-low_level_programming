74f3406de8112d5bc3a3e32a288d5513224f264d
wafaj3fr <wafaagaafar@gmail.com>
2023-08-17 15:05:37 -0700

    edits

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,37 +2,35 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: 0 is sucess
+ * fizz - prints the numbers from 1 to 100
  */
 
-int main(void)
+void fizz(void)
 {
 	int i = 1;
 
-	while (i <= 100)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-			putchar(' ');
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-			putchar(' ');
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-			putchar(' ');
-		}
-		else
-		{
-			putchar(i);
-			putchar(' ');
-		}
-		putchar('\n');
-	}
-	return (0);
+        while (i <= 100)
+        {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                        printf("FizzBuzz");
+                        putchar(' ');
+                }
+                else if (i % 3 == 0)
+                {
+                        printf("Fizz");
+                        putchar(' ');
+                }
+                else if (i % 5 == 0)
+                {
+                        printf("Buzz");
+                        putchar(' ');
+                }
+                else
+                {
+                        putchar(i);
+                        putchar(' ');
+                }
+                putchar('\n');
+        }
 }
