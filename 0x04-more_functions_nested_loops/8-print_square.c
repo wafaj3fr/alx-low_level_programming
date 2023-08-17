1460f83b2bcddeda2ff89f2f835abeba0d46de05
wafaj3fr <wafaagaafar@gmail.com>
2023-08-17 15:33:16 -0700

    editss

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,17 +10,17 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 0; i <= size; i++)
 		{
-			for (j = 1; j <= size; j++)
+			for (j = 0; j <= (size - 1); j++)
 			{
 				_putchar('#');
+				_putchar('#');
+				_putchar('\n');
 			}
+		}
+		else
+		{
 			_putchar('\n');
 		}
 	}
-	else
-	{
-		_putchar('\n');
-	}
-}
