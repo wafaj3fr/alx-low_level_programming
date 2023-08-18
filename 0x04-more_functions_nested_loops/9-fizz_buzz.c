243522f94fa6f6dd3ba1aa657760e23add81d8f8
wafaj3fr <wafaagaafar@gmail.com>
2023-08-17 18:03:07 -0700

    editing 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,17 @@
 
 int main(void)
 {
-	int i = 1;
+	int i;
 
 	while (i++ < 100)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-		{
 			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 		{
 			if (i != 100)
 				printf("Buzz ");
