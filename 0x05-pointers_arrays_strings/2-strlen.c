fef3789d5a741a7c65b56b63d24191ad21b04e65
wafaj3fr <wafaagaafar@gmail.com>
2023-08-21 15:48:18 -0700

    added 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to be calculated
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = sizeof(*s);
+	return (len);
+}
