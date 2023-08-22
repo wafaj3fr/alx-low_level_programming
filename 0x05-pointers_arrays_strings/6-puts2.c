a709378468458812c12698bf54fc09530d16da68
wafaj3fr <wafaagaafar@gmail.com>
2023-08-21 19:50:28 -0700

    added puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,17 @@
 void puts2(char *str)
 {
 	int i;
+	int len = 0;
 
-	for (i = 0; str[i] != '\0'; i+=2)
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
+
 	_putchar('\n');
 }
