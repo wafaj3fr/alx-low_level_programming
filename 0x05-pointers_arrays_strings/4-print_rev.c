a3f82c1b9f26806436c020cc9d3f934cff739e8a
wafaj3fr <wafaagaafar@gmail.com>
2023-08-21 19:14:49 -0700

    added 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** print_rev - prints a string, in reverse, followed by a new line.
+ * @s: The string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int i, j, len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	for (j = len; j > 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
