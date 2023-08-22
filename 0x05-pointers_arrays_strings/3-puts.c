5a69b7b4722eb7eeada53c309f37cfa8b0fb4e1e
wafaj3fr <wafaagaafar@gmail.com>
2023-08-21 18:23:14 -0700

    added 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: The string to be printed
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar('s[i]');
+	}
