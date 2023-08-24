4f35e42c7d2c28cbb438387dfdba2dc717aae26b
wafaj3fr <wafaagaafar@gmail.com>
2023-08-24 15:20:21 -0700

    edits

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (i < n && src[i] != '\0')
 		{
 			dest[i] = src[i];
+			i++;
 		}
 	while (i < n)
 	{
