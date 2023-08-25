115e1811ad3826d9c1dbf4187856f24d91253729
wafaj3fr <wafaagaafar@gmail.com>
2023-08-24 19:30:16 -0700

    added 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,4 +7,41 @@
 
 void print_number(int n)
 {
+	int i, j, d, s, p;
+	unsigned int temp, numchar, number;
 
+	d = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
+	else
+	{
+		temp = n;
+	}
+
+	number = temp;
+
+	while (number >= 10)
+	{
+		number = number / 10;
+		d++;
+	}
+	s = d + 1;
+	p = 1;
+	i = 1;
+
+	while (i < s)
+	{
+		p = p * 10;
+		i++;
+	}
+	j = p;
+	while (j >= 1)
+	{
+		numchar = (temp / j) % 10;
+		_putchar(numchar + '0');
+		j = j / 10;
+	}
+}
