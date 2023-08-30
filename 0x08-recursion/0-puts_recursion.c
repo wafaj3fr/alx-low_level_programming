89fdfd09259f0ca6fb0c695d67e7b09f1bae7504
wafaj3fr <wafaagaafar@gmail.com>
2023-08-30 09:00:33 -0700

    edited puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,8 +7,8 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		return ('\0')
-			_putchar(*s);
-	_puts_recursion(s + 1)
+	if (*s == 0)
+		_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
