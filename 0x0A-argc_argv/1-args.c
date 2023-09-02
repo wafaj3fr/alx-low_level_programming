dda6d4f82ecc7926d0eccd1b57be9c993c13f223
wafaj3fr <wafaagaafar@gmail.com>
2023-09-01 19:37:16 -0700

    added 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,6 +10,18 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%d", argc);
+	int i;
+
+	if (argc == 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		for (i = 0; *argv; argv++)
+		{
+			printf("%d\n", argc);
+		}
+	}
 	return (0);
 }
