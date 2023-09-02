e482cacd73885dd0de06c64e2ba301a7bf87b04e
wafaj3fr <wafaagaafar@gmail.com>
2023-09-01 20:01:32 -0700

    added 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ */
+
 int main(int argc, char *argv[])
 {
 	int i, j, add = 0;
