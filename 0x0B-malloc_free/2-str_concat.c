458ad76c753e270b13668a92078a6429770f93cd
wafaj3fr <wafaagaafar@gmail.com>
2023-09-04 13:55:52 -0700

    rewrote strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,28 +10,34 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k;
+	unsigned int i = 0, j = 0, l1 = 0, l2 = 0;
 	char *s3;
 
-	i = j = 0;
-	if (s1 != '\0')
-		for (i = 0; s1[i]; i++)
-			;
-	if (s2 != '\0')
-		for (j = 0; s2[j]; j++)
-			;
-	s3 = malloc(sizeof(char) * (i + j + 1));
-	if (s3 == 0)
+	while (s1 && s1[l1])
+		l1++;
+	while (s2 && s2[l2])
+		l2++;
+
+	s3 = malloc(sizeof(char) * (l1 + l2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-	k = 0;
-	while (k < (i + j))
+	if (s1)
 	{
-		if (k < i)
-			s3[k] = s1[k];
-		else
-			s3[k] = s2[k - i];
-		k++;
+		while (i < l1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
-	s3[k] = '\0';
+	if (s2)
+	{
+		while (i < (l1 + l2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
 	return (s3);
 }
