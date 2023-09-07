1596edee1dc7467f0a9c12755c56e6272f1e30f2
wafaj3fr <wafaagaafar@gmail.com>
2023-09-07 12:52:06 -0700

    edits

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
