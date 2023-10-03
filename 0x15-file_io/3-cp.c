af3e1a7ccdcdd1fefbb0f0103d6f7ee0187a7fa9
wafaj3fr <wafaagaafar@gmail.com>
2023-10-03 01:00:31 -0700

    edited 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,47 +1,65 @@
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "main.h"
 
-#define USAGE "Usage: cp file_from file_to\n"
-#define READ_ERR "Error: Can't read from file %s\n"
-#define WRITE_ERR "Error: Can't write to %S\n"
-#define CLOSE_ERR "Error: Can't close fd %s\n"
-#define PERMS (0664)
+void check_status(int status, const char *file_name, char perms);
 
 /**
- * main - check the code
- * @ac: argument count
- * @av: argumentvector
- * Return: Always 0.
+ * main - Copies the content of one file to another.
+ * @argc: Argument count.
+ * @argv: Arguments passed.
+ *
+ * Return: 0 on success, exit otherwise.
  */
-
-int main(int ac, char **av)
+int main(int argc, char *argv[])
 {
-	int from = 0, to = 0;
-	ssize_t b;
-	char buffer[BUFFER_SIZE];
+	int source, destination;
+	ssize_t b_read, b_wrote;
+	unsigned int perms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buffer[1024];
 
-	if (ac != 3)
-		dprintf(STDERR_FILENO, USAGE), exit(97);
-	from = open(av[1], O_RDONLY);
-	if (from == -1)
-		dprintf(STDERR_FILENO, READ_ERR, av[1]), exit(98);
-	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMS);
-	if (to == -1)
-		dprintf(STDERR_FILENO, WRITE_ERR, av[2]), exit(99);
-
-	while ((b = read(from, buffer, BUFFER_SIZE)) > 0)
+	if (argc != 3)
 	{
-		if (write(to, buffer, b) != b)
-			dprintf(STDERR_FILENO, WRITE_ERR, av[2]), exit(99);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	if (b == -1)
-		dprintf(STDERR_FILENO, READ_ERR, av[1]), exit(98);
-	from = close(from);
-	to = close(to);
-	if (from)
-		dprintf(STDERR_FILENO, CLOSE_ERR, from), exit(100);
-	if (to)
-		dprintf(STDERR_FILENO, CLOSE_ERR, from), exit(100);
 
-	return (EXIT_SUCCESS);
+	source = open(argv[1], O_RDONLY);
+	check_status(source, argv[1], 'O');
+
+	destination = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, perms);
+	check_status(destination, argv[2], 'W');
+
+	while ((b_read = read(source, buffer, sizeof(buffer))) > 0)
+	{
+		b_wrote = write(destination, buffer, b_read);
+		if (b_wrote == -1)
+			check_status(-1, argv[2], 'W');
+	}
+
+	close(source);
+	close(destination);
+
+	return (0);
 }
 
+/**
+ * check_status - Checks if a file operation was successful.
+ * @status: The status of the operation(e.g., file descriptor or return value)
+ * @file_name: Name of the file.
+ * @perms: 'O' for open, 'W' for write.
+ *
+ * Return: void.
+ */
+void check_status(int status, const char *file_name, char perms)
+{
+	if (status == -1)
+	{
+		if (perms == 'O')
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
+		else if (perms == 'W')
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
+
+		exit((perms == 'O') ? 98 : 99);
+	}
+}
