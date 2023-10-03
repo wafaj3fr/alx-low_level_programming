f88b348231c0dbcb92673e1cb00e2fb6bf235e08
wafaj3fr <wafaagaafar@gmail.com>
2023-10-02 17:03:04 -0700

    edited 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,17 +2,16 @@
 
 #define USAGE "Usage: cp file_from file_to\n"
 #define READ_ERR "Error: Can't read from file %s\n"
-#define WRITE_ERR "Error: Can't write to %S\n"
-#define CLOSE_ERR "Error: Can't close fd %s\n"
+#define WRITE_ERR "Error: Can't write to %s\n"
+#define CLOSE_ERR "Error: Can't close fd %d\n"
 #define PERMS (0664)
 
 /**
- * main - check the code
- *@ac: argument count
- @av: argumentvector
- * Return: Always 0.
+ * main - Copy the content of one file to another.
+ * @ac: Argument count
+ * @av: Argument vector
+ * Return: 0 on success, exit codes on error.
  */
-
 int main(int ac, char **av)
 {
 	int from = 0, to = 0;
@@ -20,27 +19,49 @@ int main(int ac, char **av)
 	char buffer[BUFFER_SIZE];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, USAGE), exit(97);
+	{
+		dprintf(STDERR_FILENO, USAGE);
+		exit(97);
+	}
 	from = open(av[1], O_RDONLY);
 	if (from == -1)
-		dprintf(STDERR_FILENO, READ_ERR, av[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, READ_ERR, av[1]);
+		exit(98);
+	}
 	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMS);
 	if (to == -1)
-		dprintf(STDERR_FILENO, WRITE_ERR, av[2]), exit(99);
+	{
+		dprintf(STDERR_FILENO, WRITE_ERR, av[2]);
+		exit(99);
+	}
 
 	while ((b = read(from, buffer, BUFFER_SIZE)) > 0)
 	{
 		if (write(to, buffer, b) != b)
-			dprintf(STDERR_FILENO, WRITE_ERR, av[2]), exit(99);
+		{
+			dprintf(STDERR_FILENO, WRITE_ERR, av[2]);
+			exit(99);
+		}
 	}
 	if (b == -1)
-		dprintf(STDERR_FILENO, READ_ERR, av[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, READ_ERR, av[1]);
+		exit(98);
+	}
 	from = close(from);
 	to = close(to);
-	if (from)
-		dprintf(STDERR_FILENO, CLOSE_ERR, from), exit(100);
-	if (to)
-		dprintf(STDERR_FILENO, CLOSE_ERR, from), exit(100);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, CLOSE_ERR, from);
+		exit(100);
+	}
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, CLOSE_ERR, to);
+		exit(100);
+	}
 
 	return (EXIT_SUCCESS);
 }
+
