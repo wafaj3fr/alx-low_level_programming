0b809e5f97a50be6c98180b5cb179cbe17d273f6
wafa <wafaagaafar@gmail.com>
2023-10-31 05:44:53 -0700

    updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
 
@@ -15,4 +18,3 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
-
