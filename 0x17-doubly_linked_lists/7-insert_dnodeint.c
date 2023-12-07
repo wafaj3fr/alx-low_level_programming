6734fd4c42e40fd48de9073377b684ae1673676f
wafaa <wafaagaafar@gmail.com>
2023-12-07 15:42:41 -0800

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,43 +1,68 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position
- * @h: Pointer to a pointer to the head of the list
- * @idx: Index where the new node should be added (starts at 0)
- * @n: Value to be added in the new node
- * Return: Address of the new node, or NULL if it failed
+ * create_dnode - creates new node
+ * @n: data of node
+ * @prev: link to prev node
+ * @next: link to next node
+ * Return: pointer to new node
+ */
+
+dlistint_t *create_dnode(int n, dlistint_t *prev, dlistint_t *next)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = prev;
+	new->next = next;
+	return (new);
+}
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: head of doubly-linked list
+ * @idx: index for insertion of new node
+ * @n: data for new node
+ * Return: address of new node or NULL if error
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newNode, *temp;
-	unsigned int i = 0;
+	dlistint_t *curr = *h, *localPrev = NULL;
+	unsigned int count = 0;
 
-	if (h == NULL)
+	if (!h)
 		return (NULL);
-
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	temp = *h;
-	while (temp != NULL && i < idx - 1)
 	{
-		temp = temp->next;
-		i++;
+		if (!*h)
+			*h = create_dnode(n, NULL, NULL);
+		else
+		{
+			(*h)->prev = create_dnode(n, NULL, *h);
+			*h = (*h)->prev;
+		}
+		return (*h);
 	}
-
-	if (temp == NULL || temp->next == NULL)
+	for (curr = *h; curr && (count < idx); curr = curr->next, count++)
+	{
+		localPrev = curr;
+	}
+	if ((count == idx) && (curr == NULL))
+	{
+		localPrev->next = create_dnode(n, localPrev, NULL);
+		return (localPrev->next);
+	}
+	if ((count < idx) && (curr == NULL))
 		return (NULL);
-
-	newNode = malloc(sizeof(dlistint_t));
-	if (newNode == NULL)
-		return (NULL);
-
-	newNode->n = n;
-	newNode->next = temp->next;
-	newNode->prev = temp;
-	temp->next->prev = newNode;
-	temp->next = newNode;
-
-	return (newNode);
+	if (localPrev != NULL)
+	{
+		localPrev->next = create_dnode(n, localPrev, curr);
+		curr->prev = localPrev->next;
+		return (localPrev->next);
+	}
+	return (NULL);
 }
