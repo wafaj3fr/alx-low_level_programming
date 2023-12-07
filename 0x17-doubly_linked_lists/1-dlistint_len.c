bee773600eb7acae040d733b58adc419b94623a1
wafaa <wafaagaafar@gmail.com>
2023-12-07 06:24:29 -0800

    1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Counts the number of elements in a doubly linked list
+ * @h: Pointer to the head of the list
+ * Return: Number of nodes (elements) in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return count;
+}
