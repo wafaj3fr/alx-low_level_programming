7266cb715a804e2740e3ac9f7abdbb34a87e0b92
wafaa <wafaagaafar@gmail.com>
2023-12-11 12:06:39 -0800

    0

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: the string
+ * @c: character to search for
+ * Return: pointer to first occurence of c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	if (*s == '\0')
+		return (s);
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+		return (s);
+	return ('\0');
+}
