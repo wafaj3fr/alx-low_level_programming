f67225e4145d201735b01d56837118e311e8753e
wafa <wafaagaafar@gmail.com>
2023-12-22 08:04:53 -0800

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,19 +1,90 @@
-#include "your_header_containing_structs.h"
+#include "hash_tables.h"
 
 /**
- * key_index - Calculates the index for a key in the hash table array
- * @key: The key string
- * @size: The size of the array of the hash table
- * Return: the index at which the key/value pair should be stored in the array
+ * set_pair - mallocs a key/value pair to the hash table.
+ * @key: the key.
+ * @value: the value associated with the key.
+ * Return: pointer to the new node.
+ */
+hash_node_t *set_pair(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (node == NULL)
+		return (NULL);
+	node->key = malloc(strlen(key) + 1);
+	if (node->key == NULL)
+		return (NULL);
+	node->value = malloc(strlen(value) + 1);
+	if (node->value == NULL)
+		return (NULL);
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+	return (node);
+}
+
+/**
+ * set_pair_only - (no collision) set key:value pair to first array element
+ * @ht: pointer to the hash table.
+ * @key: the key.
+ * @value: the value.
+ * @index: the key's index.
+ * Return: the node, or NULL if failed.
  */
 
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+int set_pair_only(hash_table_t *ht, const char *key,
+		  const char *value, unsigned long int index)
 {
-    if (key == NULL || size <= 0)
-        return 0; // Return 0 or any suitable value for error handling
+	hash_node_t *node = set_pair(key, value);
 
-    unsigned long int hash_val = hash_djb2(key);
-    unsigned long int index = hash_val % size;
+	if (node == NULL)
+		return (0);
+	node->next = NULL;
+	ht->array[index] = node;
+	return (1);
+}
 
-    return index;
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: a pointer to the hash table array.
+ * @key: the key.
+ * @value: the value.
+ * Return: 1 on success, 0 on error.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (key == NULL || ht == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (node == NULL)
+		return (set_pair_only(ht, key, value, index));
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			if (strcmp(node->value, value) == 0)
+				return (1);
+			free(node->value);
+			node->value = malloc(strlen(value) + 1);
+			if (node->value == NULL)
+				return (0);
+			strcpy(node->value, value);
+			return (1);
+		}
+		node = node->next;
+	}
+	if (node == NULL)
+	{
+		node = set_pair(key, value);
+		if (node == NULL)
+			return (0);
+		node->next = ht->array[index];
+		ht->array[index] = node;
+		return (1);
+	}
+	return (0);
 }
