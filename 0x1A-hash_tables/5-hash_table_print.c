c4cd23609a53966d6f39ce08d927259e16b7c749
wafa <wafaagaafar@gmail.com>
2023-12-24 08:41:50 -0800

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,57 +1,31 @@
-#ifndef __hash_tables__
-#define __hash_tables__
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "hash_tables.h"
 
 /**
- * struct hash_node_s - Node of a hash table
- *
- * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
+ * hash_table_print - prints key/value pairs in hash table array order.
+ * @ht: the hash table.
  */
-typedef struct hash_node_s
+void hash_table_print(const hash_table_t *ht)
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
-} hash_node_t;
+	unsigned long int i, count = 0;
+	hash_node_t *node;
 
-/**
- * struct hash_table_s - Hash table data structure
- *
- * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
- */
-typedef struct hash_table_s
-{
-	unsigned long int size;
-	hash_node_t **array;
-} hash_table_t;
-
-hash_table_t *hash_table_create(unsigned long int size);
-
-unsigned long int hash_djb2(const unsigned char *str);
-
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-
-hash_node_t *set_pair(const char *key, const char *value);
-
-int set_pair_only(hash_table_t *ht, const char *key, const char *value,
-		unsigned long int size);
-
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
-char *hash_table_get(const hash_table_t *ht, const char *key);
-
-void hash_table_print(const hash_table_t *ht);
-
-void hash_table_delete(hash_table_t *ht);
-
-
-#endif
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				if (count > 0)
+					printf(", ");
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				count++;
+			}
+		}
+	}
+	printf("}\n");
+}
